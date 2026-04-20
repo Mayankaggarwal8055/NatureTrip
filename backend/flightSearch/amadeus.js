5dd22b80493cb964260a9b929d14bfe004964900
Mayankaggarwal8055 <aggarwalmayank184@gmail.com>
2026-04-20 21:37:07 +0530

    bug fix

--- backend/flightSearch/amadeus.js
+++ backend/flightSearch/amadeus.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const amadeus = new Amadeus({
     clientId: process.env.AMADEUS_CLIENT_ID,
     clientSecret: process.env.AMADEUS_CLIENT_SECRET,
-    hostname: 'production'
+    // hostname: 'production' // Uncomment for live
 });
 
 module.exports = amadeus;
